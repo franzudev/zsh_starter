7b42559cbadfe067affadafc6e0df155675829ea
franzudev <fede.franzu@gmail.com>
2022-05-25 18:58:49 +0200

    init

---
+++ zsh-part1.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+sudo apt-get update
+
+sudo apt-get -y install zsh
+
+chsh -s $(which zsh)
+
+if command apt > /dev/null; then
+    
+    mkdir ~/.fonts
+
+    wget -O ~/.fonts/regular.ttf https://github.com/romkatv/powerlevel10k#:~:text=MesloLGS%20NF%20Regular.ttf
+    
+    wget -O ~/.fonts/bold.ttf https://github.com/romkatv/powerlevel10k#:~:text=MesloLGS%20NF%20Bold.ttf
+    
+    wget -O ~/.fonts/italic.ttf https://github.com/romkatv/powerlevel10k#:~:text=MesloLGS%20NF%20Italic.ttf
+    
+    wget -O ~/.fonts/bold_italic.ttf https://github.com/romkatv/powerlevel10k#:~:text=MesloLGS%20NF%20Bold%20Italic.ttf
+
+elif [[ `uname` == "Darwin" ]]; then
+
+    wget -O ~/Library/Fonts/regular.ttf https://github.com/romkatv/powerlevel10k#:~:text=MesloLGS%20NF%20Regular.ttf
+    
+    wget -O ~/Library/Fonts/bold.ttf https://github.com/romkatv/powerlevel10k#:~:text=MesloLGS%20NF%20Bold.ttf
+    
+    wget -O ~/Library/Fonts/italic.ttf https://github.com/romkatv/powerlevel10k#:~:text=MesloLGS%20NF%20Italic.ttf
+    
+    wget -O ~/Library/Fonts/bold_italic.ttf https://github.com/romkatv/powerlevel10k#:~:text=MesloLGS%20NF%20Bold%20Italic.ttf
+
+fi
+
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
---
+++ zsh-part2.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+sed -i 's|ZSH_THEME=.*|ZSH_THEME="powerlevel10k/powerlevel10k"|g' ~/.zshrc
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+git clone https://github.com/ptavares/zsh-kubectx.git ~/.oh-my-zsh/custom/plugins/zsh-kubectx
+
+sed -i 's/plugins=(git)/plugins=(git kubectl helm zsh-syntax-highlighting zsh-autosuggestions zsh-kubectx)/g' ~/.zshrc
+
